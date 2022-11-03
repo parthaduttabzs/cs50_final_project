4b276959d8eb7a23f7b9f762c4276547473e52ec
Partha Dutta <parthaduttabzs@gmail.com>
2022-11-03 23:59:28 +0530

    testing source

--- test_python.py
+++ test_python.py
@@ -1,4 +1,8 @@
-import datetime
+from datetime import datetime
 
+x = datetime.now()
+# format_data = "%Y-%m-%d %H:%M:%S:%f"
+# date = datetime.strptime(x, "%Y-%m-%d")
 print("Hello from ubuntu server running on windows")
-print(datetime.datetime.now())
+# print(f"today is {date.date} of month: {date.month} in the year: {date.year}")
+print(x)
