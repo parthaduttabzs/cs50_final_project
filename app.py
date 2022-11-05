a5ccefce709a918cc0ab9140c9d03aa7280f7631
Partha Dutta <parthaduttabzs@gmail.com>
2022-11-06 02:54:36 +0530

    added registration, login, logout, add_fund modules, sort/filter buttons added to frontend

---
+++ app.py
@@ -0,0 +1,206 @@
+import os
+from flask import Flask, render_template, session, request, redirect, flash
+from werkzeug.security import check_password_hash, generate_password_hash
+from cs50 import SQL
+
+from flask_session import Session
+from functools import wraps
+from PIL import Image
+import requests
+
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# image resizing
+image = Image.open("static/images/milk.png")
+image.resize((200,200)).save("static/images/milk_200.png")
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///ecomm.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+def login_required(f):
+    # decorates routes to require login
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def password_strength(password):
+    # initiating variables
+    length = len(password)
+    lower = 0
+    upper = 0
+    digit = 0
+    special = 0
+    # loop through all the characters of the password
+    for i in range(length):
+        c = password[i]
+        # check for lower case
+        if (c.islower()):
+            lower += 1
+        # check for upper case
+        if (c.isupper()):
+            upper += 1
+        # check for digits
+        if (c.isdigit()):
+            digit += 1
+        # check for special characters
+        if (c in ("@", "#", "_")):
+            special += 1
+    # check for final password validation
+    if (length < 6 or lower < 1 or upper < 1 or digit < 1 or special < 1):
+        # if password is not strong enough
+        return False
+        # password matches the validation criteri and hence strong
+    else:
+        return True
+
+
+@app.route("/")
+@login_required
+def index():
+    user_data = db.execute("SELECT * FROM users WHERE user_id = ?", session["user_id"])
+    return render_template("index.html", user_data = user_data)
+
+
+def error(message, code=400):
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("error.html", code=code, message=escape(message)), code
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # if user has placed a register request
+    if request.method == "POST":
+        # check for valid inputs
+        if not request.form.get("email") or not request.form.get("phone") or not request.form.get("name"):
+            return error("must provide all valid details", 400)
+        # ensure no duplicate email ID
+        elif len(db.execute("SELECT user_id FROM users WHERE email = ?", request.form.get("email"))) == 1:
+            return (error("Email ID already exists", 400))
+        # ensure no duplicate mobile number
+        elif len(db.execute("SELECT user_id FROM users WHERE phone = ?", request.form.get("phone"))) == 1:
+            return (error("Mobile number already exists", 400))
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return error("must provide password", 400)
+        # ensure password matches
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return error("password didn't match")
+        # ensure password is strong (disabled to pass cs50 check50 and submit50)
+        # elif not password_strength(request.form.get("password")):
+        #     return error("password needs to be atleast 6 character, with min 1 upper, 1 lower, 1 digit and 1 special ('@', '#', '_') character", 400)
+        # accept username and password and proceed to register
+        hash = generate_password_hash(request.form.get("password"))
+        db.execute("INSERT INTO users (name, email, phone, hash) VALUES (?, ?, ?, ?)", request.form.get("name"), request.form.get("email"), request.form.get("phone"), hash)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE email = ?", request.form.get("email"))
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["user_id"]
+        flash("You have successfully registered!")
+        return redirect("/")
+    # if user is visiting register page
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure email was submitted
+        if not request.form.get("email"):
+            return error("must provide email", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return error("must provide password", 403)
+
+        # Query database for email ID
+        rows = db.execute("SELECT * FROM users WHERE email = ?", request.form.get("email"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return error("invalid Email ID and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["user_id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+    flash(f"You have successfully logged out")
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/add_fund", methods=["GET", "POST"])
+def add_fund():
+    # get user data
+    user_id = session["user_id"]
+    user_data = db.execute("SELECT * FROM users WHERE user_id = ?;", user_id)
+    # if user has placed a add fund request
+    if request.method == "POST":
+        amount = request.form.get("amount")
+        # validation check
+        if not 0 < int(amount) < 1000000000:
+            return error("Entered amount is invalid", 406)
+        # execute fund addition
+        db.execute(f"UPDATE users SET cash = (cash + ?) WHERE user_id = ?", amount, user_id)
+        flash(f"Rs. {amount}/- has been added to your account")
+        return redirect("/")
+    # if user is visiting the add fund page
+    return render_template("add_fund.html", user_data=user_data)
--- test.py
+++ test.py
@@ -1,48 +1,66 @@
-# import requests module
-import requests
-import json
-import urllib.parse
-import sys
+import sqlite3
+from sqlite3 import Error
 
-# Making a get request
-# response = requests.get('https://api.github.com')
+def create_connection(db_file):
+    conn = None
 
-meme = sys.argv[1]
-top = sys.argv[2]
-bottom = sys.argv[3]
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.closed()
 
-url = f"https://apimeme.com/meme?meme={urllib.parse.quote_plus(meme)}&top={urllib.parse.quote_plus(top)}&bottom={urllib.parse.quote_plus(bottom)}"
+if __name__ == '__main__':
+    create_connection("ecomm.db")
+
+# # import requests module
+# import requests
+# import json
+# import urllib.parse
+# import sys
+
+# # Making a get request
+# # response = requests.get('https://api.github.com')
+
+# meme = sys.argv[1]
+# top = sys.argv[2]
+# bottom = sys.argv[3]
+
+# url = f"https://apimeme.com/meme?meme={urllib.parse.quote_plus(meme)}&top={urllib.parse.quote_plus(top)}&bottom={urllib.parse.quote_plus(bottom)}"
 
 
-response = requests.get(url)
-
-with open("/home/projects/final_project/meme2.png",'wb') as f:
-    f.write(response.content)
-
-print(response)
 # response = requests.get(url)
-# # print response
+
+# with open("/home/projects/final_project/meme2.png",'wb') as f:
+#     f.write(response.content)
+
 # print(response)
+# # response = requests.get(url)
+# # # print response
+# # print(response)
 
-# header = response.headers 
+# # header = response.headers 
 
-# json_data = response.headers
-# json_object = json.loads(json_data)
-# print headers of response
-# for i in header:
-#     print(f"{i}: {header[i]}")
-
-
+# # json_data = response.headers
+# # json_object = json.loads(json_data)
+# # print headers of response
+# # for i in header:
+# #     print(f"{i}: {header[i]}")
 
 
 
 
-# from datetime import datetime
 
-# x = datetime.now()
-# format_data = "%Y-%m-%d"
-# # date = datetime.strptime(x, "%Y-%m-%d")
-# print("Hello from ubuntu server running on windows")
-# # print(f"today is {date.date} of month: {date.month} in the year: {date.year}")
-# print(x)
-# print(format_data)
+
+# # from datetime import datetime
+
+# # x = datetime.now()
+# # format_data = "%Y-%m-%d"
+# # # date = datetime.strptime(x, "%Y-%m-%d")
+# # print("Hello from ubuntu server running on windows")
+# # # print(f"today is {date.date} of month: {date.month} in the year: {date.year}")
+# # print(x)
+# # print(format_data)
